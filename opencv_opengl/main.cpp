b17cd322f385401c4afdecf90042e12e75a77939
ZENJU Daisuke <yoshizumi@zoho.com>
2015-09-30 11:10:31 +0900

    Initial Commit

---
+++ opencv_opengl/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_opengl
+//
+//  Created by ZENJU Daisuke on 2015/09/30.
+//  Copyright © 2015年 ZENJU Daisuke. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
